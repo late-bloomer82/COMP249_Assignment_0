6033faec27b006a88b69c9cfe0e27df51d830482
late-bloomer82 <nateriver72@outlook.com>
2025-09-18 15:04:31 -0400

    javadoc documentation

--- src/Appliance.java
+++ src/Appliance.java
@@ -1,3 +1,14 @@
+/**
+ * -----------------------------------------------------
+ * Assignment 0
+ * Question: Part 1
+ * Written by: Ahmad-Radjai Cherifi, 40327936
+ * -----------------------------------------------------
+ * This program defines an Appliance class for household appliances with the following attributes:
+ * a unique serial number, type, brand and price.
+ * It tracks the total number of appliances created, and has methods for attribute access, modification, comparison, and display.
+ */
+
 public class Appliance {
  private String type;
  private String brand;
@@ -6,9 +17,11 @@ public class Appliance {
  private double price;
  private static int count= 0;
 
+ /** Constructor creates object, validates inputs type and sets attributes values */
  public Appliance(String type, String brand, double price) {
   if (!isValidType(type)) throw new IllegalArgumentException("Invalid appliance type");
   if (price < 1) throw new IllegalArgumentException("Price must be at least 1$");
+
   this.type = type;
   this.brand = brand;
   this.price = price;
@@ -16,12 +29,13 @@ public class Appliance {
   count++;
 
  }
+
  private static final String[] VALID_TYPES = {
          "Fridge", "Air Conditioner", "Washer", "Dryer",
          "Freezer", "Stove", "Dishwasher", "Water Heaters", "Microwave"
  };
 
-// Helper method to validate appliance type
+/** Helper method to validate appliance type */
  private boolean isValidType(String type) {
   for (String t : VALID_TYPES) {
    if (t.equals(type)) return true;
@@ -30,7 +44,7 @@ public class Appliance {
  }
 
 
- // Accessors
+ /** Accessors */
  public String getType() {
   return this.type;
  }
@@ -47,7 +61,7 @@ public class Appliance {
   return this.price;
  }
 
- // Mutators
+ /** Mutators */
  public void setType(String type) {
   if(!isValidType(type)){throw new IllegalArgumentException("Invalid appliance type");}
   this.type = type;
@@ -62,7 +76,7 @@ public class Appliance {
   this.price = price;
  }
 
- // Printing the object's information
+ /** Printing the object's information */
  @Override
  public String toString() {
   return "Appliance [Type: " + type +
@@ -71,11 +85,12 @@ public class Appliance {
           ", Price: $" + price + "]";
  }
 
+/** Static method to track number of created appliances */
  public static int findNumberOfCreatedAppliances(){
  return count;
  }
 
-
+/** Comparing objects equality */
  public boolean equals(Appliance obj){
   return this.type.equals(obj.type) && this.brand.equals(obj.brand) && this.price == obj.price;
  }
