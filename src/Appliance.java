58e955bb032c53b12ef6b158aa68c6292f6ab6de
late-bloomer82 <nateriver72@outlook.com>
2025-09-18 13:51:38 -0400

    added toString, a findNumberOfCreatedAppliances method and an equals method

--- src/Appliance.java
+++ src/Appliance.java
@@ -3,38 +3,60 @@ public class Appliance {
  private String brand;
  final long serial_no;
  private double price;
+ private static int count= 0;
 
-  public Appliance(String type, String brand, long serial_no, double price){
-   this.type = type;
-   this.brand = brand;
-   this.serial_no= serial_no;
-   this.price = price;
+ public Appliance(String type, String brand, long serial_no, double price) {
+  this.type = type;
+  this.brand = brand;
+  this.serial_no = serial_no;
+  this.price = price;
+  count++;
 
+ }
 
-  }
-  //Accessors
-  public String getType(){
-   return this.type;
-  }
- public String getBrand(){
+ //Accessors
+ public String getType() {
+  return this.type;
+ }
+
+ public String getBrand() {
   return this.brand;
  }
- public long getSerial_no(){
+
+ public long getSerial_no() {
   return this.serial_no;
  }
- public double getPrice(){
+
+ public double getPrice() {
   return this.price;
  }
 
  //Mutators
- public void setType(String type){
-   this.type = type;
+ public void setType(String type) {
+  this.type = type;
  }
- public void setBrand(String brand){
+
+ public void setBrand(String brand) {
   this.brand = brand;
  }
- public void setPrice(double price){
+
+ public void setPrice(double price) {
   this.price = price;
  }
 
+ //Printing the object's information
+ public String toString() {
+  return "Appliance [Type: " + type +
+          ", Brand: " + brand +
+          ", Serial: " + serial_no +
+          ", Price: $" + price + "]";
+ }
+
+ public static int findNumberOfCreatedAppliances(){
+ return count;
+ }
+
+ public boolean equals(Appliance other){
+  return this.type.equals(other.type) && this.brand.equals(other.brand) && this.price == other.price;
+ }
 }
